9067b84fe063191a74b7f4e7fe984dc55fb49c38
rahaf fallatah <rahaf77553@gmail.com>
2025-04-07 23:53:18 +0300

    Additional modifications to files and database

--- Muwazir/main/models.py
+++ Muwazir/main/models.py
@@ -2,22 +2,23 @@ from django.db import models
 
 # Create your models here.
 class user(models.Model):
-    full_name=models.CharField(max_length=512)
+    full_name=models.CharField(max_length=100)
     phone_num=models.IntegerField()
-    gender=models.CharField(max_length=512 ,default='M')
+    gender=models.CharField(max_length=100,default='M')
     email=models.EmailField()
     nationality=models.CharField(max_length=512) 
 
 class volunteer(models.Model):
-    full_name=models.CharField(max_length=512)
+    full_name=models.CharField(max_length=100)
     phone_num=models.IntegerField()
-    gender=models.CharField(max_length=512 , default='M')
+    gender=models.CharField(max_length=100,default='M')
     email=models.EmailField()
-    nationality=models.CharField(max_length=512)
+    nationality=models.CharField(max_length=100)
     experiences=models.TextField()
-    service=models.CharField(max_length=512)
-    is_available=models.BooleanField()
-    is_verified=models.BooleanField()
+    service=models.CharField(max_length=100)
+    Volunteering_site=models.CharField(max_length=100,default="The holy mosque")
+    is_available=models.BooleanField(default=True)
+    is_verified=models.BooleanField(default=False)
     ratings=models.SmallIntegerField(default=0)
 
 
