8bcd81a0f5c794a8418358c37b7aa93142442e88
rahaf fallatah <rahaf77553@gmail.com>
2025-04-08 01:59:35 +0300

    Additional modifications to the files

--- Muwazir/volunteers/views.py
+++ Muwazir/volunteers/views.py
@@ -4,6 +4,10 @@ from main.models import volunteer
 
 # Create your views here.
 def create_volunteer_account(request:HttpRequest):
+    if request.method=="POST":
+        new_volunteer=volunteer(full_name=request.POST["full_name"],gender=request.POST["gender"],nationality=request.POST["nationality"],phone_num=request.POST["phone_num"],email=request.POST["email"],Volunteering_site=request.POST["Volunteering_site"],service=request.POST["service"])
+
+
     
     return render(request, "volunteers/create.html")
 
