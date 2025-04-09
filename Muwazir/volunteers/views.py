a2d07c98730c678b674ef74fba634ee442a9b2f2
rahaf fallatah <rahaf77553@gmail.com>
2025-04-09 22:40:53 +0300

    Additional modifications to the files

--- Muwazir/volunteers/views.py
+++ Muwazir/volunteers/views.py
@@ -7,8 +7,6 @@ def create_volunteer_account(request:HttpRequest):
     if request.method=="POST":
         new_volunteer=volunteer(full_name=request.POST["full_name"],gender=request.POST["gender"],nationality=request.POST["nationality"],phone_num=request.POST["phone_num"],email=request.POST["email"],Volunteering_site=request.POST["Volunteering_site"],service=request.POST["service"],profile_photo=request.FILES["profile_photo"])
         new_volunteer.save()
-
-
         return redirect('main:home')
 
     return render(request, "volunteers/create.html")
@@ -27,6 +25,10 @@ def volunteer_profile(request:HttpRequest ,volunteer_id:int):
 
 
     return render(request, "volunteers/profileh.html",{"volunteers":volunteer})
+
+def update_profile(request:HttpRequest ,volunteer_id:int):
+
+    return render(request, "volunteers/update_profile.html",{"volunteers":volunteer})
 
 
     
